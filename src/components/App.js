5357eb925907e9b37f4ed828837279050e25096d
Kanagaraj <kanagarajwhb@gmail.com>
2021-11-29 23:06:05 +0530

    youtube api clone using function component

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
   componentDidMount = () => {
-    this.onTermSearch("cars");
+    this.onTermSearch("buildings");
   };
 
   onTermSearch = async (term) => {
